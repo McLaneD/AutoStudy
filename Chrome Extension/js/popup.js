037229ec2309f57bcbf86e1c29066100e33f102e
aaron <zhfyfd@gmail.com>
2020-03-05 15:27:35 +0800

    .

--- Chrome Extension/js/popup.js
+++ Chrome Extension/js/popup.js
@@ -2,7 +2,7 @@
 
 var autoStudy_popup = angular.module("autoStudy_popup", []);
 
-autoStudy_popup.controller("popupCtrl", function ($scope, $http) {
+autoStudy_popup.controller("popupCtrl", function ($scope, $http, $httpParamSerializerJQLike) {
 
     $scope.test = function () {
         chrome.notifications.create(null, {
@@ -11,5 +11,25 @@ autoStudy_popup.controller("popupCtrl", function ($scope, $http) {
             title: "标题",
             message: "付款成功!"
         });
-    };
+    }
+
+    $scope.mockLogin = function () {
+        $http({
+            method: "POST", url: "https://linan.learning.gov.cn/study/login.php",
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+            data: $httpParamSerializerJQLike({
+                act: "AjaxLogin",
+                username: $scope.loginUser.username,
+                password: $scope.loginUser.password,
+                islogin: 0,
+                authKey: $scope.loginUser.captcha
+            })
+        }).success((response) => {
+            console.log("success");
+            console.log(response);
+        }).error((response) => {
+            console.log("error");
+            console.log(response);
+        });
+    }
 });
